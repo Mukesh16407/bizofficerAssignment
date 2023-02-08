7f9303ed1b9be511378379e4686e15924086af5d
Mukesh16407 <mukesh.16407@gmail.com>
2023-02-08 13:10:35 +0530

    change-check-box

--- src/components/Tables.jsx
+++ src/components/Tables.jsx
@@ -1,36 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { DataGrid } from '@mui/x-data-grid';
-import FavoriteBorder from '@mui/icons-material/FavoriteBorder';
-import Favorite from '@mui/icons-material/Favorite';
+import Radio from "@mui/material/Radio";
 
-const columns = [
-  { field: '', headerName: '', width: 100 },
-  { field: 'id', headerName: '#', width: 100 },
-  { field: 'casenumber', headerName: 'Case Number', width: 130 },
-  { field: 'title', headerName: 'Title', width: 130 },
-  {field:"product",headerName:"Product",width:130},
-  {field:"substatus", headerName:"Sub Status", width:130},
-  {field:"status", headerName:"Status", width:130},
-  {field:"technician", headerName:"Technician", width:130},
-  {field:"technicianStatus", headerName:"Technician Status", width:130},
-  {field:"techniciandatetime", headerName:"Technician date Time", width:130},
-
-  {field:"accountNumber", headerName:"AccountNumber", width:130},
-  {field:"contactName", headerName:"Contact Name", width:130},
-  {field:"mobile", headerName:"Mobile", width:130},
-  {field:"phone", headerName:"Phone", width:130},
-  {field:"address", headerName:"Address", width:130},
-  {field:"package", headerName:"Package", width:130},
-
-  {field:"billable", headerName:"Billable", width:130},
-  {field:"amount", headerName:"Amount", width:130},
-  {field:"priority", headerName:"Priority", width:130},
-  {field:"agent", headerName:"Agent", width:130},
-  {field:"channel", headerName:"Channel", width:130},
-  {field:"remarks", headerName:"Remarks", width:130},
-  {field:"comments", headerName:"Comments", width:130},
-  
-];
 const rows = [
   { id: 1, casenumber: 111116, title: 'Incidence', product: "Air Conditioner",substatus:"Cancelled", status:"Closed",technician:"Mohan Mahalotra",
   technicianStatus:"New",techniciandatetime:"28-01-2023 3:30pm",accountNumber:"Ok limited",contactName:"Jai Mal",mobile:9086754321,phone:6543218,address:"K.K 333",
@@ -58,16 +29,63 @@ package:"",billable:"",amount:"",priority:"High",agent:"Andrew Wiston",channel:"
 technicianStatus:"New",techniciandatetime:"28-01-2023 3:30pm",accountNumber:"Ok limited",contactName:"Jai Mal",mobile:9686754321,phone:6543218,address:"K.K 333",
 package:"",billable:"",amount:"",priority:"High",agent:"Andrew Wiston",channel:"Primary",remarks:"",comments:""
 },
+ 
 ];
+let radioChecked = [rows[0].id];
+const columns = [
+  {
+    field: "radiobutton",
+    headerName: "",
+    width: 100,
+    sortable: false,
+    renderCell: (params) => (
+      <Radio checked={radioChecked[0] === params.id} value={params.id} />
+    )
+  },
+  { field: 'casenumber', headerName: 'Case Number', width: 130 },
+  { field: 'title', headerName: 'Title', width: 130 },
+  {field:"product",headerName:"Product",width:130},
+  {field:"substatus", headerName:"Sub Status", width:130},
+  {field:"status", headerName:"Status", width:130},
+  {field:"technician", headerName:"Technician", width:130},
+  {field:"technicianStatus", headerName:"Technician Status", width:130},
+  {field:"techniciandatetime", headerName:"Technician date Time", width:130},
+
+  {field:"accountNumber", headerName:"AccountNumber", width:130},
+  {field:"contactName", headerName:"Contact Name", width:130},
+  {field:"mobile", headerName:"Mobile", width:130},
+  {field:"phone", headerName:"Phone", width:130},
+  {field:"address", headerName:"Address", width:130},
+  {field:"package", headerName:"Package", width:130},
+
+  {field:"billable", headerName:"Billable", width:130},
+  {field:"amount", headerName:"Amount", width:130},
+  {field:"priority", headerName:"Priority", width:130},
+  {field:"agent", headerName:"Agent", width:130},
+  {field:"channel", headerName:"Channel", width:130},
+  {field:"remarks", headerName:"Remarks", width:130},
+  {field:"comments", headerName:"Comments", width:130},
+];
+
+
 export const Tables = () => {
+  const [selectionModel, setSelectionModel] = useState(radioChecked);
+  radioChecked = selectionModel;
+
+  const selectedRow = rows.filter((item) => {
+    return item.id === selectionModel[0];
+  });
+
   return (
     <div style={{ height: 400, width: '100%',marginTop:"15px" }}>
       <DataGrid
         rows={rows}
         columns={columns}
-        pageSize={5}
-        rowsPerPageOptions={[5]}
-        checkboxSelection={true}
+        autoHeight
+        selectionModel={selectionModel}
+        onSelectionModelChange={(newSelectionModel) => {
+          setSelectionModel(newSelectionModel);
+        }}
         
       />
     </div>
